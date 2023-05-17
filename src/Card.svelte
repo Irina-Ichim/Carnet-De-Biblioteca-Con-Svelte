7617fb6b333a0e2700ae25b1d9339c92e30e19c5
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-05-17 13:56:37 +0200

    agregando Card.svelte+import App.svelte

---
+++ src/Card.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let nombreCompleto;
+  let firstName = '';
+  let lastName = '';
+
+  $: {
+    const names = nombreCompleto.split(' ');
+    firstName = names[0] || '';
+    lastName = names.slice(1).join(' ') || '???';
+  }
+</script>
+
+<style>
+  .card {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+</style>
+
+<div class="card">
+  <h3>Card Component</h3>
+  <p>nom: {firstName}</p>
+  <p>cognoms: {lastName}</p>
+</div>
