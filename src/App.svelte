4c4b1e2d16f62d63c70c540d212b4bfdd9251647
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-05-17 14:53:59 +0200

    implementando segundo ejercicio nom: Loli cognoms: ???

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,41 @@
 <script>
   import Card from './Card.svelte';
 
-  let nombreCompleto = 'Pepa Fuentes Martínez';
+  //let nombreCompleto = "Pepa Fuentes Martínez";
+let nombreCompleto = "Loli"
 </script>
 
-<div>
+<style>
+  /*body {
+    background-color: black;
+    color: white;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+  }*/
+
+  h1 {
+    font-size: 32px;
+    text-align: center;
+    margin-bottom: 20px;
+    color: #FF6B6B;
+  }
+
+  .card {
+    border: 1px solid white;
+    padding: 20px;
+    background: linear-gradient(to bottom, #FF6B6B, #FF8E53);
+    color: white;
+  }
+</style>
+
+<h1>IRINA</h1>
+
+<div class="card">
+  <h3>Card Component</h3>
   <Card nombreCompleto={nombreCompleto} />
 </div>
+
--- src/Card.svelte
+++ src/Card.svelte
@@ -1,25 +1,17 @@
 <script>
-  export let nombreCompleto;
-  let firstName = '';
-  let lastName = '';
-
-  $: {
-    const names = nombreCompleto.split(' ');
-    firstName = names[0] || '';
-    lastName = names.slice(1).join(' ') || '???';
-  }
-</script>
-
-<style>
-  .card {
-    border: 1px solid #ccc;
-    padding: 10px;
-    margin-bottom: 10px;
-  }
-</style>
-
-<div class="card">
-  <h3>Card Component</h3>
+    export let nombreCompleto;
+  
+    let firstName = '';
+    let lastName = '';
+  
+    $: {
+      const names = nombreCompleto.split(' ');
+      firstName = names[0] || '';
+      lastName = names.slice(1).join(' ') || '???';
+    }
+  </script>
+  
   <p>nom: {firstName}</p>
   <p>cognoms: {lastName}</p>
-</div>
+  
+  
